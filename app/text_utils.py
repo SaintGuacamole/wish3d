65dc3ada20ccb5f229da850ebd41dff216f9f02e
Raphi <raphi_winkler@hotmail.de>
2023-12-11 21:49:38 +0100

    - changed regex to capture any number

--- app/text_utils.py
+++ app/text_utils.py
@@ -3,7 +3,7 @@ import os
 import re
 from typing import List, Union
 
-pattern = r"(\d+)\s+"
+pattern = r"(zero|one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen|fourteen|fifteen|sixteen|seventeen|eighteen|ninceteen|twenty|\d+)\s+"
 
 preamble = r'(show me|give me|show|give|generate|create|create for me)\s+'
 as_pattern = r'\ba\b\s+(\w+)'
@@ -19,6 +19,31 @@ def clean_prompt(prompt: str, remove_a_s: bool = False) -> str:
     return prompt
 
 
+numbers = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+    "twenty"
+]
+
+
 def split_prompt(prompt: str, task_base_path: Union[str, None]) -> List[str]:
     _prompt = clean_prompt(prompt)
     prompts = _prompt.split(" and ")
@@ -27,16 +52,22 @@ def split_prompt(prompt: str, task_base_path: Union[str, None]) -> List[str]:
     for prompt in prompts:
         prompt = prompt.replace(",", "").replace(".", "").replace("?", "").replace("!", "")
 
-        match = re.search(pattern, prompt)
+        _match = re.search(pattern, prompt)
+        if _match:
+            try:
+                num = int(_match.group(1))
+            except ValueError:
+                if numbers.index(_match.group(1)) > -1:
+                    num = numbers.index(_match.group(1))
+                else:
+                    num = 1
 
-        if match:
-            num = int(match.group(1))
             prompt = re.sub(pattern, "", prompt)
             prompt = prompt.strip()
         else:
             num = 1
 
-        num = max(0, min(num, 20))
+        num = max(1, min(num, 20))
         tasks.extend([prompt] * num)
 
     if task_base_path:
